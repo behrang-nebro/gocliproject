e448857c1628a7096d986cad7f7864eb3a43862f
behrang <behrangchaichi.coding@gmail.com>
2025-11-17 15:38:10 +0330

    I changed the structure this seems more logical for bigger projects

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 
-	mathops "github.com/behrang-nebro/calculator/internal"
+	mathops "github.com/behrang-nebro/calculator/internal/mathhops"
 )
 
 func main() {
 	fmt.Println("hello this is calc main")
-	result := mathops.Add(2, 3)
+	result := mathops.Add(10, 3.6)
 	fmt.Println("the add operation:", result)
-	result = mathops.Sub(10, 4)
+	result = mathops.Sub(10, 4.75)
+	fmt.Println("the sub operation:", result)
+	result = mathops.Mul(3.65, 4)
+	fmt.Println("the sub operation:", result)
+	result = mathops.Div(23.21, 5.6598)
 	fmt.Println("the sub operation:", result)
 }
