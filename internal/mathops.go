864a7d33a09a7c5e2bb268edd1893ba128dc7f23
behrang <behrangchaichi.coding@gmail.com>
2025-11-17 15:09:01 +0330

    now the git is updated

---
+++ internal/mathops.go
@@ -0,0 +1,12 @@
+package mathops
+
+import "fmt"
+
+func Add(x, y int) int {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x + y
+}
+func Sub(x, y int) int {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x - y
+}
