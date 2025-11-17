a929dacafdbb232c0560b740536d12109d7197fa
behrang <behrangchaichi.coding@gmail.com>
2025-11-17 15:06:27 +0330

    now the structure is working fine

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	mathops "github.com/behrang-nebro/calculator/internal"
 )
 
 func main() {
 	fmt.Println("hello this is calc main")
+	result := mathops.Add(2, 3)
+	fmt.Println("the add operation:", result)
+	result = mathops.Sub(10, 4)
+	fmt.Println("the sub operation:", result)
 }
