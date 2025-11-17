4aa395649459bd6b3ed36de2f08deea0e2d0c993
behrang <behrangchaichi.coding@gmail.com>
2025-11-17 15:38:33 +0330

    I changed the structure this seems more logical for bigger projects

---
+++ internal/mathhops/mathops.go
@@ -0,0 +1,20 @@
+package mathops
+
+import "fmt"
+
+func Add(x, y float64) float64 {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x + y
+}
+func Sub(x, y float64) float64 {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x - y
+}
+func Mul(x, y float64) float64 {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x * y
+}
+func Div(x, y float64) float64 {
+	fmt.Println("this code id runing from ./internal/mathops/")
+	return x / y
+}
