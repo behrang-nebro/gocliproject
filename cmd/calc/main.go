5027ca0def9278a90f2a955ffe5a30ab602d7880
behrang <behrangchaichi.coding@gmail.com>
2025-11-17 16:46:50 +0330

    now I implemeted the parsing of the CL. now I can do the operations on the command line

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	mathops "github.com/behrang-nebro/calculator/internal/mathhops"
 )
 
 func main() {
-	fmt.Println("hello this is calc main")
-	result := mathops.Add(10, 3.6)
-	fmt.Println("the add operation:", result)
-	result = mathops.Sub(10, 4.75)
-	fmt.Println("the sub operation:", result)
-	result = mathops.Mul(3.65, 4)
-	fmt.Println("the sub operation:", result)
-	result = mathops.Div(23.21, 5.6598)
-	fmt.Println("the sub operation:", result)
+	// fmt.Println("hello this is calc main")
+	// result := mathops.Add(10, 3.6)
+	// fmt.Println("the add operation:", result)
+	// result = mathops.Sub(10, 4.75)
+	// fmt.Println("the sub operation:", result)
+	// result = mathops.Mul(3.65, 4)
+	// fmt.Println("the mul operation:", result)
+	// result = mathops.Div(23.21, 5.6598)
+	// fmt.Println("the div operation:", result)
+	// result = mathops.Sin(30)
+	// fmt.Println("the sin operation:", result)
+	// result = mathops.Cosin(60)
+	// fmt.Println("the cosin operation:", result)
+	op := flag.String("op", "", "the operations: add, sub, mul, div, sin, cos")
+	x := flag.Float64("x", 0, "first number")
+	y := flag.Float64("y", 0, "second number")
+
+	flag.Parse()
+
+	var result float64
+
+	switch *op {
+	case "add":
+		result = mathops.Add(*x, *y)
+	case "sub":
+		result = mathops.Sub(*x, *y)
+	case "mul":
+		result = mathops.Mul(*x, *y)
+	case "div":
+		result = mathops.Div(*x, *y)
+	case "sin":
+		result = mathops.Sin(*x)
+	case "cos":
+		result = mathops.Cosin(*x)
+	default:
+		fmt.Println("Unknown operation: ", *op)
+	}
+
+	fmt.Println("result:", result)
 }
--- internal/mathhops/mathops.go
+++ internal/mathhops/mathops.go
@@ -1,20 +1,30 @@
 package mathops
 
-import "fmt"
+import (
+	"math"
+)
 
 func Add(x, y float64) float64 {
-	fmt.Println("this code id runing from ./internal/mathops/")
+	// fmt.Println("this code id runing from ./internal/mathops/")
 	return x + y
 }
 func Sub(x, y float64) float64 {
-	fmt.Println("this code id runing from ./internal/mathops/")
+	// fmt.Println("this code id runing from ./internal/mathops/")
 	return x - y
 }
 func Mul(x, y float64) float64 {
-	fmt.Println("this code id runing from ./internal/mathops/")
+	// fmt.Println("this code id runing from ./internal/mathops/")
 	return x * y
 }
 func Div(x, y float64) float64 {
-	fmt.Println("this code id runing from ./internal/mathops/")
+	// fmt.Println("this code id runing from ./internal/mathops/")
 	return x / y
+}
+func Sin(x float64) float64 {
+	// fmt.Println("this code id runing from ./internal/mathops/")
+	return math.Sin((x / 180) * math.Pi)
+}
+func Cosin(x float64) float64 {
+	// fmt.Println("this code id runing from ./internal/mathops/")
+	return math.Cos((x / 180) * math.Pi)
 }
